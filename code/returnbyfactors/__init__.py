bda1c8265ba2bcafc292a8010e022fb157cceb3d
İbrahim Kaplan <ibrahimckaplan@gmail.com>
2019-01-17 10:03:05 +0300

    version 0.2

--- code/returnbyfactors/__init__.py
+++ code/returnbyfactors/__init__.py
@@ -7,9 +7,8 @@
 import pandas as pd
 import numpy as np
 
-def analyze_by_score(price_data,factor_score):
-    pivotdata = price_data.pivot_table(values='adjusted_close',index='date',columns='symbol')
-
+def analyze_by_score(pivotdata,factor_score):
+   
     returns = pivotdata.pct_change()
            
     scores = factor_score
@@ -49,7 +48,7 @@ def analyze_by_score(price_data,factor_score):
         return subgroup
     for i in range(1,6):
         groups.append(groupper(i*1.0))
-    
+    odayreturns
     # group every companies values by day
     # [group_no][1,3,5][comp_pos]
     def dailygroupreturns(day):  
@@ -63,6 +62,8 @@ def analyze_by_score(price_data,factor_score):
                 count=0
                 #if a title of a company from group array is equal to 
                 #odayreturns tables columns, add the value of this companies return into returnsarray'day_range'
+                
+                
                 for j in odayreturns.columns:
                     if j==i:
                         returnsarray1.append(odayreturns.values[day][count])
@@ -92,80 +93,46 @@ def analyze_by_score(price_data,factor_score):
     #final array has indexes such as [day][group_ind][returnbyday][comp_index]
     farray = []
     #day,group_ind,[1,3,5],comp_ind
-    t=len(returns.index)
+    #t=len(returns.index)
+    t=len(groups[0])
     for k in range(t):
-        farray.append(dailygroupreturns(k))
+    	farray.append(dailygroupreturns(k))
     
     #creating dataframes for each group, includes 1day,3day,5day returns columns
-    group1returnby1 =[]
-    for i in farray:
-        group1returnby1.append(np.mean(i[0][0]))
-    group1returnby3 =[]
-    for i in farray:
-        group1returnby3.append(np.mean(i[0][1]))
-    group1returnby5 =[]
-    for i in farray:
-        group1returnby5.append(np.mean(i[0][2]))
-        
-    f = {'1dayreturn': group1returnby1  , '3dayreturn':group1returnby3 , '5dayreturn':group1returnby5 ,'index' :returns.index}
+  
+   #[groupno][1_3_5]
+    grouped_returns=[]
+    for j in range(5):
+        returnby1 = []
+        returnby3 = []
+        returnby5 = []
+        for i in farray:
+            returnby1.append(np.mean(i[j][0]))
+            returnby3.append(np.mean(i[j][1]))
+            returnby5.append(np.mean(i[j][2]))
+        greturns = [returnby1,returnby3,returnby5]   
+        grouped_returns.append(greturns)
+
+    f = {'1dayreturn': grouped_returns[0][0] , '3dayreturn':grouped_returns[0][1] , '5dayreturn':grouped_returns[0][2] ,'index' :len(groups[0])}
     finaldfg1 =pd.DataFrame(data=f)
     finaldfg1['group_title'] = 'Group1'
-    
-    group2returnby1 =[]
-    for i in farray:
-        group2returnby1.append(np.mean(i[1][0]))
-    group2returnby3 =[]
-    for i in farray:
-        group2returnby3.append(np.mean(i[1][1]))
-    group2returnby5 =[]
-    for i in farray:
-        group2returnby5.append(np.mean(i[1][2]))    
-    f = {'1dayreturn': group2returnby1  , '3dayreturn':group2returnby3 , '5dayreturn':group2returnby5 ,'index' :returns.index}
+
+    f = {'1dayreturn': grouped_returns[1][0] , '3dayreturn':grouped_returns[1][1] , '5dayreturn':grouped_returns[1][2] ,'index' :len(groups[1])}
     finaldfg2 =pd.DataFrame(data=f)
     finaldfg2['group_title'] = 'Group2'
-   
-    
-    group3returnby1 =[]
-    for i in farray:
-        group3returnby1.append(np.mean(i[2][0]))
-    group3returnby3 =[]
-    for i in farray:
-        group3returnby3.append(np.mean(i[2][1]))
-    group3returnby5 =[]
-    for i in farray:
-        group3returnby5.append(np.mean(i[2][2]))  
-    f = {'1dayreturn': group3returnby1  , '3dayreturn':group3returnby3 , '5dayreturn':group3returnby5 ,'index' :returns.index}
+  
+    f = {'1dayreturn': grouped_returns[2][0] , '3dayreturn':grouped_returns[2][1] , '5dayreturn':grouped_returns[2][2] ,'index' :len(groups[2])}
     finaldfg3 =pd.DataFrame(data=f)
     finaldfg3['group_title'] = 'Group3'
-    
-    
-    group4returnby1 =[]
-    for i in farray:
-        group4returnby1.append(np.mean(i[3][0]))
-    group4returnby3 =[]
-    for i in farray:
-        group4returnby3.append(np.mean(i[3][1]))
-    group4returnby5 =[]
-    for i in farray:
-        group4returnby5.append(np.mean(i[3][2]))  
-    f = {'1dayreturn': group4returnby1  , '3dayreturn':group4returnby3 , '5dayreturn':group4returnby5 ,'index' :returns.index}
+
+    f = {'1dayreturn': grouped_returns[3][0] , '3dayreturn':grouped_returns[3][1] , '5dayreturn':grouped_returns[3][2] ,'index' :len(groups[3])}
     finaldfg4 =pd.DataFrame(data=f)
     finaldfg4['group_title'] = 'Group4'
-  
-    
-    group5returnby1 =[]
-    for i in farray:
-        group5returnby1.append(np.mean(i[4][0]))
-    group5returnby3 =[]
-    for i in farray:
-        group5returnby3.append(np.mean(i[4][1]))
-    group5returnby5 =[]
-    for i in farray:
-        group5returnby5.append(np.mean(i[4][2]))    
-    f = {'1dayreturn': group5returnby1  , '3dayreturn':group5returnby3 , '5dayreturn':group5returnby5 ,'index' :returns.index}
+
+    f = {'1dayreturn': grouped_returns[4][0] , '3dayreturn':grouped_returns[4][1] , '5dayreturn':grouped_returns[4][2] ,'index' :len(groups[4])}
     finaldfg5 =pd.DataFrame(data=f)
     finaldfg5['group_title'] = 'Group5'
-    
+
     #merging all grouping into one dataframe
     frames = [finaldfg1,finaldfg2,finaldfg3,finaldfg4,finaldfg5]
     result = pd.concat(frames)
@@ -192,7 +159,6 @@ def analyze_by_score(price_data,factor_score):
       groupno= 'Group'+ str(count+1)  
       d5.append(resultreverseindexed['5dayreturn'][groupno].mean())  
       count=count+1
-
     
     fres = {'1dayreturn':d1   , '3dayreturn':d3 , '5dayreturn':d5, 'index':['Group1','Group2','Group3','Group4','Group5']}
     finalresult =pd.DataFrame(data=fres)
@@ -203,6 +169,3 @@ def analyze_by_score(price_data,factor_score):
     finalpivot['5dayreturn'] = finalpivot['5dayreturn'] - finalpivot['5dayreturn'].mean()
     
     return finalpivot
-    
-   
-          
---
+++ code/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='returnanalysis',
+	version='0.1',
+	description='Analyzing by factors',
+	url='https://github.com/ibrahimkaplan2015400102/CrossSectionalReturn',
+	author='Ibrahim Kaplan',
+	author_email='ibrahimckaplan@gmail.com',
+	license='MIT',
+	packages=['returnbyfactors'],
+	zip_safe=False
+	)

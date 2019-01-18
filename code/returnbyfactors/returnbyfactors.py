d67b073e36c15030901ddd719c88600cfb92c2b6
İbrahim Kaplan <ibrahimckaplan@gmail.com>
2019-01-18 17:00:44 +0300

    version 0.5

--- code/returnbyfactors/returnbyfactors.py
+++ code/returnbyfactors/returnbyfactors.py
@@ -5,78 +5,68 @@
 """
 
 import pandas as pd
-import numpy as np
 
-def calc_daybased_return(pivotdata):
-    odayreturns = pivotdata.pct_change(periods=1) 
-    odayreturns = odayreturns.shift(periods=-1)
+def calc_daybased_return(pivotdata,day):
 
-    threedayreturns = pivotdata.pct_change(periods=3)
-    threedayreturns = threedayreturns.shift(periods=-3)
+    dayrangedreturns = pivotdata.pct_change(periods=day).shift(periods=day*-1)
+
+    return dayrangedreturns
+
+def prepare_for_join(pivot,day_range):
     
-    fivedayreturns = pivotdata.pct_change(periods=5)
-    fivedayreturns = fivedayreturns.shift(periods=-5)
-
-    return odayreturns,threedayreturns,fivedayreturns
-
-def prepare_for_join(firstpivot,secondpivot,thirdpivot):
-    dfodayreturns = firstpivot.unstack()
-    dfodayreturns = dfodayreturns.reset_index(level=[0,1])
-    dfodayreturns.columns = ['symbol', 'date', '1dayreturns']
-    dfodayreturns = dfodayreturns.set_index(['date','symbol'])
-        
-    dfthreedayreturns = secondpivot.unstack()
-    dfthreedayreturns = dfthreedayreturns.reset_index(level=[0,1])
-    dfthreedayreturns.columns = ['symbol', 'date', '3dayreturns']
-    dfthreedayreturns = dfthreedayreturns.set_index(['date','symbol'])
-        
-    dffivedayreturns = thirdpivot.unstack()
-    dffivedayreturns = dffivedayreturns.reset_index(level=[0,1])
-    dffivedayreturns.columns = ['symbol', 'date', '5dayreturns']
-    dffivedayreturns = dffivedayreturns.set_index(['date','symbol'])
+    df_dayranged_returns = pivot.unstack()
+    df_dayranged_returns = df_dayranged_returns.reset_index(level=[0,1])
+    df_dayranged_returns.columns = ['symbol', 'date', str(day_range)+'dayreturn']
+    df_dayranged_returns = df_dayranged_returns.set_index(['date','symbol'])
     
-    return dfodayreturns,dfthreedayreturns,dffivedayreturns
+    return df_dayranged_returns
 
-def analyze_by_score(pivotdata,factor_score):
-           
-    scores = factor_score
-    grouplabeled = scores
+def analyze_by_score(pivotdata,factor_score,nofgroups=5,return_range=[1,5,10],):
+    
+    grouplabeled = factor_score
    
-    #grouping companies in pivot table every day based on their factor_scores
     count=0
     for i in grouplabeled.values:   
-        k = pd.qcut(i,5,labels=[1,2,3,4,5],duplicates='drop')
+        k = pd.qcut(i,nofgroups,labels=list(range(1,nofgroups+1)),duplicates='drop')
         grouplabeled.values[count] = k
         count=count+1
-    #calculating returns by given day range
+    
     groupdf = grouplabeled.unstack()
     groupdf = groupdf.reset_index(level=[0,1])
     groupdf.columns = ['symbol', 'date', 'group number']  
     groupdf = groupdf.set_index(['date','symbol'])
     
-    odayreturns,threedayreturns,fivedayreturns = calc_daybased_return(pivotdata) 
-    
-    first_df,third_df,fifth_df = prepare_for_join(odayreturns,threedayreturns,fivedayreturns) 
-    
-    totaldata= groupdf.join(first_df)
-    totaldata= totaldata.join(third_df)
-    totaldata = totaldata.join(fifth_df)
+    day_ranged_returns_pivot = []
+    for i in return_range:
+        day_ranged_returns_pivot.append(calc_daybased_return(pivotdata,i))
 
-    onedayreturns = []
-    threedayreturns = []
-    fivedayreturns = []
-    for i in range(1,6):
-        onedayreturns.append(totaldata.loc[totaldata['group number'] == i*1.0]['1dayreturns'].mean())
-        threedayreturns.append(totaldata.loc[totaldata['group number'] == i*1.0]['3dayreturns'].mean())
-        fivedayreturns.append(totaldata.loc[totaldata['group number'] == i*1.0]['5dayreturns'].mean())
+    day_ranged_returns_df = []
+    for i in return_range:
+        day_ranged_returns_df.append(prepare_for_join
+                                     (day_ranged_returns_pivot[return_range.index(i)],i))
+                            
     
-    fres = {'1dayreturn':onedayreturns  , '3dayreturn':threedayreturns , '5dayreturn':fivedayreturns, 'index':['Group1','Group2','Group3','Group4','Group5']}
-    finalresult =pd.DataFrame(data=fres)
-    finalpivot = finalresult.set_index('index')
-  
+    total_df = groupdf.join(day_ranged_returns_df)   
     
-    finalpivot['1dayreturn'] = finalpivot['1dayreturn'] - finalpivot['1dayreturn'].mean()
-    finalpivot['3dayreturn'] = finalpivot['3dayreturn'] - finalpivot['3dayreturn'].mean()
-    finalpivot['5dayreturn'] = finalpivot['5dayreturn'] - finalpivot['5dayreturn'].mean()
+    day_ranged_returns_results = []                                    
     
-    return finalpivot
+    for j in return_range:
+        for i in range(1,nofgroups+1):
+            day_ranged_returns_results.append(total_df.loc
+                                                          [total_df['group number'] == i*1.0]
+                                                                                             [str(j)+'dayreturn'].mean())
+            
+    result_dict={}
+    keys = [ str(j)+'dayreturn' for j in return_range ]
+    
+    for i in keys:
+        result_dict.update({i:day_ranged_returns_results[:nofgroups]})
+        day_ranged_returns_results = day_ranged_returns_results[nofgroups:]
+    index_list= [ 'Group'+str(j) for j in range(1,nofgroups+1)] 
+    result_dict.update({'index': index_list})
+    factor_scores = pd.DataFrame(data=result_dict).set_index('index')
+   
+    for q in return_range:
+        factor_scores[str(q)+'dayreturn'] = factor_scores[str(q)+'dayreturn'] - factor_scores[str(q)+'dayreturn'].mean()
+
+    return factor_scores
